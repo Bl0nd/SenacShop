058cf8f12b2a6e14209267919751585c5e9b2276
FERNANDA ARAUJO <fernanda.adesanto@senacsp.edu.br>
2024-05-29 22:02:37 -0300

    controller

--- SenacShop.Console/Program.cs
+++ SenacShop.Console/Program.cs
@@ -4,64 +4,80 @@ using SenacShop.DAL.DTO;
 int opcao;
 do
 {
-    Console.WriteLine("Escolha uma opção: ");
-    Console.WriteLine("1 - Cadastrar Produto");
-    Console.WriteLine("2 - Listar Produtos");
-    Console.WriteLine("9 - Sair ");
+    MostrarMenu();
     opcao = Convert.ToInt32(Console.ReadLine());
 
     if (opcao == 1)
     {
-        Console.Write("Digite o nome do produto: ");
-        string nome = Console.ReadLine();
-
-        Console.Write("Digite a descrição do produto: ");
-        string descricao = Console.ReadLine();
-
-        Console.Write("Digite o preço: ");
-        decimal preco = Convert.ToDecimal(Console.ReadLine());
-
-        Console.Write("Digite a quantidade em estoque: ");
-        int estoque = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Digite o código da categoria: ");
-        //carregar as categorias
-        CategoriaDal operacoesCategoria = new CategoriaDal();
-        List<CategoriaDto> categorias = operacoesCategoria.Listar();
-        foreach (CategoriaDto item in categorias)
-        {
-            Console.WriteLine($"{item.id} - {item.nome}");
-        }
-        int categoria = Convert.ToInt32(Console.ReadLine());
-
-
-        ProdutoDto produto = new ProdutoDto();
-        produto.nome = nome;
-        produto.descricao = descricao;
-        produto.preco = preco;
-        produto.estoque = estoque;
-        produto.idCategoria = categoria;
-
-        ProdutoDal operacoesProduto = new ProdutoDal();
-        operacoesProduto.Cadastrar(produto);
-        Console.WriteLine("Produto cadastrado com sucesso !");
+        CadastrarProduto();
     }
 
     if (opcao == 2)
     {
-        //carregar os produtos
-        ProdutoDal operacoesProduto = new ProdutoDal();
-
-        List<ProdutoDto> produtos = operacoesProduto.Listar();
-        foreach (ProdutoDto item in produtos)
-        {
-            Console.WriteLine("---------------");
-            Console.WriteLine($"Código: {item.idProduto}");
-            Console.WriteLine($"Nome: {item.nome}");
-            Console.WriteLine($"Descição: {item.descricao}");
-            Console.WriteLine($"Preço: {item.preco}");
-            Console.WriteLine($"Estoque: {item.estoque}");
-        }
+        ListarProdutos();
     }
 }
 while (opcao != 9);
+
+static void MostrarMenu()
+{
+    Console.WriteLine("Escolha uma opção: ");
+    Console.WriteLine("1 - Cadastrar Produto");
+    Console.WriteLine("2 - Listar Produtos");
+    Console.WriteLine("9 - Sair ");
+}
+
+static void CadastrarProduto()
+{
+    Console.Write("Digite o nome do produto: ");
+    string nome = Console.ReadLine();
+
+    Console.Write("Digite a descrição do produto: ");
+    string descricao = Console.ReadLine();
+
+    Console.Write("Digite o preço: ");
+    decimal preco = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Digite a quantidade em estoque: ");
+    int estoque = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Digite o código da categoria: \n");
+    //carregar as categorias
+    CategoriaDal operacoesCategoria = new CategoriaDal();
+    List<CategoriaDto> categorias = operacoesCategoria.Listar();
+    foreach (CategoriaDto item in categorias)
+    {
+        Console.WriteLine($"{item.id} - {item.nome}");
+    }
+    int categoria = Convert.ToInt32(Console.ReadLine());
+
+
+    ProdutoDto produto = new ProdutoDto();
+    produto.nome = nome;
+    produto.descricao = descricao;
+    produto.preco = preco;
+    produto.estoque = estoque;
+    produto.idCategoria = categoria;
+
+    ProdutoDal operacoesProduto = new ProdutoDal();
+    operacoesProduto.Cadastrar(produto);
+    Console.WriteLine("Produto cadastrado com sucesso !");
+    Console.WriteLine("       ");
+}
+
+static void ListarProdutos()
+{
+    //carregar os produtos
+    ProdutoDal operacoesProduto = new ProdutoDal();
+
+    List<ProdutoDto> produtos = operacoesProduto.Listar();
+    foreach (ProdutoDto item in produtos)
+    {
+        Console.WriteLine("---------------");
+        Console.WriteLine($"Código: {item.idProduto}");
+        Console.WriteLine($"Nome: {item.nome}");
+        Console.WriteLine($"Descição: {item.descricao}");
+        Console.WriteLine($"Preço: {item.preco}");
+        Console.WriteLine($"Estoque: {item.estoque}");
+    }
+}
---
+++ SenacShop.Web/Controllers/ProdutoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SenacShop.Web;
+
+namespace SenacShop.Web.Controllers
+{
+    public class ProdutoesController : Controller
+    {
+        private SenacShopEntities db = new SenacShopEntities();
+
+        // GET: Produtoes
+        public ActionResult Index()
+        {
+            var produto = db.Produto.Include(p => p.Categoria);
+            return View(produto.ToList());
+        }
+
+        // GET: Produtoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produtoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCategoria = new SelectList(db.Categoria, "idCategoria", "nome");
+            return View();
+        }
+
+        // POST: Produtoes/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idProduto,nome,descricao,preco,estoque,idCategoria")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produto.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCategoria = new SelectList(db.Categoria, "idCategoria", "nome", produto.idCategoria);
+            return View(produto);
+        }
+
+        // GET: Produtoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCategoria = new SelectList(db.Categoria, "idCategoria", "nome", produto.idCategoria);
+            return View(produto);
+        }
+
+        // POST: Produtoes/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idProduto,nome,descricao,preco,estoque,idCategoria")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCategoria = new SelectList(db.Categoria, "idCategoria", "nome", produto.idCategoria);
+            return View(produto);
+        }
+
+        // GET: Produtoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produtoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto produto = db.Produto.Find(id);
+            db.Produto.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
